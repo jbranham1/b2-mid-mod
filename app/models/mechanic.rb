896e144f50615d1f5b58147ab655547050aa73e0
Jenny Branham <branhamjenny@gmail.com>
2021-02-22 10:36:38 -0600

    Add average method to mechanics

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,5 +2,9 @@ class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
 
-  validates :name, presence: true, uniqueness:true
+  validates :name, presence: true, uniqueness: true
+
+  def self.average_experience
+    average(:years_of_experience)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Mechanic, type: :model do
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of :name }
   end
+
+  describe 'class methods' do
+    describe "::average_experience" do
+      it "returns average experience of all mechanics" do
+        mechanic1 = Mechanic.create(name: 'Joe Bob', years_of_experience: 10)
+        mechanic2 = Mechanic.create(name: 'Kara Smith', years_of_experience: 7)
+        expect(Mechanic.average_experience).to eq(8.5)
+      end
+    end
+  end
 end
