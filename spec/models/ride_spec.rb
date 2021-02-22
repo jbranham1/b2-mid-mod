401459e593ffcd7cf222c82dd715f46e482fecd6
Jenny Branham <branhamjenny@gmail.com>
2021-02-22 10:20:13 -0600

    Add Models and tests

---
+++ spec/models/ride_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Ride, type: :model do
+  describe 'relationships' do
+    it { should have_many :mechanic_rides }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+  end
+end
